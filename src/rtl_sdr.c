986c63b2d222062d1f764ae848bae61c684cd407
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2024-03-08 12:21:42 +0100

    If gain is negative, activate a software AGC to get this level at IQ level. Also change sample counter to 64-bit int to allow for more than 2^31 samples.

--- src/rtl_sdr.c
+++ src/rtl_sdr.c
@@ -21,6 +21,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
 
 #ifndef _WIN32
 #include <unistd.h>
@@ -39,8 +41,30 @@
 #define MINIMAL_BUF_LENGTH		512
 #define MAXIMAL_BUF_LENGTH		(256 * 16384)
 
+#define DBFS_MIN (-480)
+
+// Look Up Table in 10*dBFS
+static int dbfs_lut[256][256];
+
+static void dbfs_lut_init(void){
+	int i, q;
+	for(i = 0 ; i < 256 ; i++){
+		int centered_i = i - 158;
+		int centered_i_squared = centered_i * centered_i;
+		for(q = 0 ; q < 256 ; q++){
+			int centered_q = q - 128;
+			int mag = centered_i_squared + (centered_q * centered_q);
+			int dbfs = DBFS_MIN; // if mag is 0
+			if(mag > 0){
+				dbfs = 100 * logf((float)mag / 16384.0); // 0dBFS => <||I||=128, ||Q||=0> or <||I||=0, ||Q||=128> ; ||I|| & ||Q|| = 128 => 3dBFS
+			}
+			dbfs_lut[i][q] = dbfs;
+		}
+	}
+}
+
 static int do_exit = 0;
-static uint32_t bytes_to_read = 0;
+static uint64_t bytes_to_read = 0;
 static rtlsdr_dev_t *dev = NULL;
 
 void usage(void)
@@ -82,25 +106,145 @@ static void sighandler(int signum)
 }
 #endif
 
+#define MAX_HZ (5)
+static int target_dbfs = 0; // in tenth of dBFS
+static int current_gain = 0; // in tenth of dB
+static uint32_t samp_rate = DEFAULT_SAMPLE_RATE;
+static int max_samples = 0;
+static int sample_index = 0;
+static int max_dbfs = DBFS_MIN;
+#include <sys/eventfd.h>
+int max_event = -1;
+int delta_gain = 0;
+int pid_somme = 0;
+
+int gain_count = 0;
+int gains[100];
+int gain_index = 1;
+
+static void initialize_gains(void){
+	int i;
+	gain_count = rtlsdr_get_tuner_gains(dev, NULL);
+	fprintf(stderr, "Supported gain values (%d): ", gain_count);
+
+	gain_count = rtlsdr_get_tuner_gains(dev, gains);
+	for (i = 0; i < gain_count; i++)
+		fprintf(stderr, "%.1f ", gains[i] / 10.0);
+	fprintf(stderr, "\n");
+	gain_index = (gain_count / 2);
+}
+
+void update_gain_pid(int delta){
+	int old_pid_somme = pid_somme;
+	pid_somme += delta;
+	// fprintf(stderr, "%s:somme+delta %d+%d=>%d" "\n", __func__, old_pid_somme, delta, pid_somme);
+}
+
+static void signal_event(int fd){
+	uint64_t u = 1ULL;
+	write(fd, &u, sizeof(u));
+}
+
+static void clear_event(int fd){
+	uint64_t u = 1ULL;
+	read(fd, &u, sizeof(u));
+}
+
+static int create_event(void){
+	return(eventfd(0, EFD_NONBLOCK));
+}
+
+static void *set_new_gain_thread(void *unused){
+	fprintf(stderr, "%s started !" "\n", __func__);
+	do{
+		fd_set wait;
+		struct timeval timeout;
+		int selected = 0;
+		FD_ZERO(&wait);
+		FD_SET(max_event, &wait);
+		timeout.tv_usec = 0;
+		timeout.tv_sec = 1;
+		selected = select(max_event + 1, &wait, NULL, NULL, &timeout);
+		if(selected > 0){
+			int new_gain_index = gain_index;
+			clear_event(max_event);
+			if(pid_somme > 300){
+				pid_somme -= 300;
+				new_gain_index++;
+			}
+			if(pid_somme < -300){
+				pid_somme += 300;
+				new_gain_index--;
+			}
+			// Ensure new_gain_index points to a usable gain
+			if(new_gain_index < 1){
+				new_gain_index = 1;
+			}else if(new_gain_index > (gain_count - 1)){
+				new_gain_index = (gain_count - 1);
+			}
+			if(new_gain_index != gain_index){
+				current_gain =  gains[new_gain_index];
+				// fprintf(stderr, "%s: target_dbfs = %d, gain_index:%d=>%d (gains:%d=>%d) ; ", __func__,  target_dbfs, gain_index, new_gain_index, gains[gain_index], gains[new_gain_index]);
+				verbose_gain_set(dev, current_gain);
+				gain_index = new_gain_index;
+			}
+		}
+	}while(0 == do_exit);
+	return(unused);
+}
+
+static void max_thread_init(void){
+	pthread_t max_thread_id;
+	max_event = create_event();
+	pthread_create(&max_thread_id, NULL, set_new_gain_thread, NULL);
+}
+
+
 static void rtlsdr_callback(unsigned char *buf, uint32_t len, void *ctx)
 {
 	if (ctx) {
 		if (do_exit)
 			return;
 
-		if ((bytes_to_read > 0) && (bytes_to_read < len)) {
+		if ((bytes_to_read > 0) && (bytes_to_read < (uint64_t)len)) {
 			len = bytes_to_read;
 			do_exit = 1;
 			rtlsdr_cancel_async(dev);
 		}
+		if(target_dbfs < 0) {
+			/* Compute MAX for sample_rate / MAX_HZ IQ samples */
+			int n = len / 2;
+			while(n--){
+				unsigned char *p = buf;
+				unsigned char i, q;
+				int level;
+				if(sample_index > 0){
+					sample_index--;
+				}else{
+					delta_gain = target_dbfs - max_dbfs;
+					update_gain_pid(delta_gain);
+					signal_event(max_event);
+					// fprintf(stderr, "%s: target_dbfs=%d, max_dbfs=%d, delta=%d" "\n", __func__, target_dbfs, max_dbfs, delta_gain);
+					
+					sample_index = max_samples - 1;
+					max_dbfs = DBFS_MIN;
+				}
+				i = *p++;
+				q = *p++;
+				level = dbfs_lut[i][q];
+				if(level > max_dbfs){
+					max_dbfs = level;
+				}
 
+			}
+		}
 		if (fwrite(buf, 1, len, (FILE*)ctx) != len) {
 			fprintf(stderr, "Short write, samples lost, exiting!\n");
 			rtlsdr_cancel_async(dev);
 		}
 
 		if (bytes_to_read > 0)
-			bytes_to_read -= len;
+			bytes_to_read -= (uint64_t)len;
 	}
 }
 
@@ -121,7 +265,6 @@ int main(int argc, char **argv)
 	int dev_index = 0;
 	int dev_given = 0;
 	uint32_t frequency = 100000000;
-	uint32_t samp_rate = DEFAULT_SAMPLE_RATE;
 	uint32_t out_block_size = DEFAULT_BUF_LENGTH;
 
 	while ((opt = getopt(argc, argv, "d:f:g:s:b:n:p:SD")) != -1) {
@@ -146,7 +289,7 @@ int main(int argc, char **argv)
 			out_block_size = (uint32_t)atof(optarg);
 			break;
 		case 'n':
-			bytes_to_read = (uint32_t)atof(optarg) * 2;
+			bytes_to_read = (uint64_t)atof(optarg) * 2;
 			break;
 		case 'S':
 			sync_mode = 1;
@@ -218,9 +361,22 @@ int main(int argc, char **argv)
 		 /* Enable automatic gain */
 		verbose_auto_gain(dev);
 	} else {
-		/* Enable manual gain */
-		gain = nearest_gain(dev, gain);
-		verbose_gain_set(dev, gain);
+		if(gain > 0){
+			/* Enable manual gain */
+			gain = nearest_gain(dev, gain);
+			verbose_gain_set(dev, gain);
+		}else{
+			/* Enable manual AGC */
+			/* target is in tenth of dBFS */
+			initialize_gains();
+			current_gain = gains[gain_index];
+			verbose_gain_set(dev, current_gain);
+			target_dbfs = gain;
+			max_samples = samp_rate / MAX_HZ;
+			sample_index = max_samples;
+			dbfs_lut_init();
+			max_thread_init();
+		}
 	}
 
 	verbose_ppm_set(dev, ppm_error);
